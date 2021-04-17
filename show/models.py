4b955c94a5f2dfdbea20458f140abf6134304433
TanLuong <luongnhattan2807@gmail.com>
2021-04-18 06:33:48 +0700

    haha

--- show/models.py
+++ show/models.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 
 # Create your models here.
 class ieltsstories(models.Model):
+    id = models.IntegerField(blank=True, primary_key=True)
     title = models.TextField(blank=True, null=True)
     content = models.TextField(blank=True, null=True)
 
@@ -12,15 +13,14 @@ class ieltsstories(models.Model):
     def __str__(self):
         return self.title
 
-
 class ieltstest(models.Model):
-    sublevel = models.IntegerField(db_column='SubLevel', blank=True, null=True)  # Field name made lowercase.
-    qnumber = models.IntegerField(db_column='QNumber', blank=True, primary_key=True)  # Field name made lowercase.
-    qcontent = models.TextField(db_column='QContent', blank=True, null=True)  # Field name made lowercase.
-    answera = models.TextField(db_column='AnswerA', blank=True, null=True)  # Field name made lowercase.
-    answerb = models.TextField(db_column='AnswerB', blank=True, null=True)  # Field name made lowercase.
-    answerc = models.TextField(db_column='AnswerC', blank=True, null=True)  # Field name made lowercase.
-    answerd = models.TextField(db_column='AnswerD', blank=True, null=True)  # Field name made lowercase.
+    sublevel = models.IntegerField(blank=True, null=True)  # Field name made lowercase.
+    qnumber = models.IntegerField(blank=True, primary_key=True)  # Field name made lowercase.
+    qcontent = models.TextField(blank=True, null=True)  # Field name made lowercase.
+    answera = models.TextField(blank=True, null=True)  # Field name made lowercase.
+    answerb = models.TextField(blank=True, null=True)  # Field name made lowercase.
+    answerc = models.TextField(blank=True, null=True)  # Field name made lowercase.
+    answerd = models.TextField(blank=True, null=True)  # Field name made lowercase.
     correctanswer = models.TextField(db_column='CorrectAnswer', blank=True, null=True)  # Field name made lowercase.
     passed = models.IntegerField(db_column='Passed', blank=True, null=True)  # Field name made lowercase.
 
